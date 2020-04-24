3721efd92f612ef73405d2a77e73a9ba5c98534b
bmeelawn <budhathokimilan4@gmail.com>
2020-04-24 13:40:20 +0545

    df

--- application/api/common/establishment.php
+++ application/api/common/establishment.php
@@ -5,12 +5,12 @@ header('Access-Control-Allow-Method:GET');
 header('Content-Type: application/json');
 
 include '../../includes/autoloader-class.php';
-include '../../includes/utility.php';
+// include '../../includes/utility.php';
 
-if(authAPI()) {
+// if(authAPI()) {
 $category = new EstablishmentView();
 echo $category->showAllEstablishment();
-}
+// }
 
 
 
