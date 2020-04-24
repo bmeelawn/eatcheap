cc415f11ab4505881f6fb4714d0900bd9724c3f7
bmeelawn <budhathokimilan4@gmail.com>
2020-04-24 14:29:37 +0545

    change api key

--- application/api/common/restaurant.php
+++ application/api/common/restaurant.php
@@ -4,14 +4,15 @@ ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:GET');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: *');
 
 include '../../includes/autoloader-class.php';
 include '../../includes/utility.php';
 
-// if(authAPI()) {
+if(authAPI()) {
 $restaurant = new RestaurantView();
 echo $restaurant->showAllRestaurant();
-// }
+}
 
 
 
