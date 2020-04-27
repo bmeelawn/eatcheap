894d153cfc80aa366201e1c1cf6459fdb53b3316
bmeelawn <budhathokimilan4@gmail.com>
2020-04-27 15:03:53 +0545

    added code

---
+++ application/view/reviewView.php
@@ -0,0 +1,44 @@
+<?php
+
+include '../../model/reviewModel.php';
+
+class ReviewView extends ReviewModel {
+    private $data = array();
+
+   public function showAllReviews($res_id) {
+        $result = $this->getReviews($res_id);
+        $count = $this->getCount();
+
+        if($count > 0) {
+            $this->data['reviews'] =[]; 
+        // Fetch Categories
+        foreach($result as $key=>$values) {
+
+            $id = ['restaurant_id' => $result[$key]['restaurant_id']];
+            $arr = [
+                'comment' => $result[$key]['review'],
+                'name' => $result[$key]['name']
+            ];
+        // Push Data Into Data Array
+        array_push($this->data['reviews'], $arr);
+        }
+           // Set HTTP Response -200 OK
+           http_response_code(200);
+           // Show Data in JSON Format
+           return json_encode([
+               "status" => true,
+               "message" => "Data is found.",
+               "data" => array_merge($id, $this->data)  
+               ]);
+       } else {
+           // Set HTTP Response -404 ERROR
+           http_response_code(404);
+           // Show Data in JSON Format
+           return json_encode([
+               "status" => false,
+               "message" => "No data is found.",
+               "data" => $this->data    
+               ]);
+       }
+       }
+   }
