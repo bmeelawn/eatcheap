9ea6a91fb8240a2f2579590c0f7d54dce3cd2f05
bmeelawn <budhathokimilan4@gmail.com>
2020-04-27 15:21:17 +0545

    added code

--- application/api/common/restaurantreview.php
+++ application/api/common/restaurantreview.php
@@ -4,9 +4,7 @@ ini_set('display_errors', 1);
 
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:GET');
-header('Content-Type: application/json');
-header('Content-Type: text/html; charset=utf-8');
-
+header('Content-Type: application/json, text/html; charset=utf-8');
 header('Access-Control-Allow-Headers: *');
 
 include '../../includes/autoloader-class.php';
