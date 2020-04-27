64c55e6e3628d7f8d6b9a024d39ff78c09b66fd1
bmeelawn <budhathokimilan4@gmail.com>
2020-04-27 15:18:30 +0545

    added code

--- application/api/common/restaurantreview.php
+++ application/api/common/restaurantreview.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:GET');
 header('Content-Type: application/json');
-header('Content-Type: text/html: charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 
 header('Access-Control-Allow-Headers: *');
 
