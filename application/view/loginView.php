45722a57cf9109e54af6155155a3f72787a2bdb6
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 13:16:57 +0545

    added code

---
+++ application/view/loginView.php
@@ -0,0 +1,49 @@
+<?php
+include '../../model/loginModel.php';
+
+class LoginView extends LoginModel {
+    private $error;
+    private $arr=[];
+    public function viewUserDetails($username, $password) {
+        $getUserDetails = $this->authUser($username);
+
+        if($getUserDetails) {
+            $passFromDb = $getUserDetails['password'];
+            if($passFromDb === $password) {
+                // Get image
+                $userImg= $getUserDetails['image'];
+                $imagePath = 'https'. '://' . $_SERVER['HTTP_HOST'] . '/images/users/';
+                $filePath = '../../../images/users/';
+                if(file_exists($filePath . $userImg)) {
+                   $userImgLoc = $imagePath . $userImg;
+                } else {
+                    $userImgLoc = null;
+                }   
+
+                $this->arr['firstname'] = $getUserDetails['firstname'];
+                $this->arr['lastname'] = $getUserDetails['lastname'];
+                $this->arr['username'] = $getUserDetails['username'];
+                $this->arr['email'] = $getUserDetails['email'];
+                $this->arr['image'] = $userImgLoc;
+
+                return json_encode([
+                    "status" => true,
+                    "message" => "User is Authenticated",
+                    "data" => $this->arr
+                ]);
+                
+            } else {
+                $this->error = "Incorrect Password.";
+            }
+        } else {
+            $this->error = "Incorrect Username.";
+        }  
+
+        return json_encode([
+            "status" => false,
+            "message" => $this->error,
+            "data" => $this->arr
+        ]);
+
+    }
+}
