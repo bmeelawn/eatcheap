8f36db55f0e1d8ca2960a18955bd3af46e8d1ece
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 08:05:18 +0545

    added code

--- application/view/dealView.php
+++ application/view/dealView.php
@@ -4,6 +4,7 @@ include '../../model/dealModel.php';
 
 class DealView extends DealModel {
     private $data = array();
+    private $arrR;
     public function showAllDeals() {
        $results =  $this->getDeals();
        $count = $this->getCount();
@@ -12,58 +13,16 @@ class DealView extends DealModel {
        // Fetch Deals Data
        foreach($results as $key => $values) {
 
-        // Get Review for a Restaurant
-        $id = $results[$key]['res_id'];
-        $reviews = $this->getReviews($id);
-        $countReview = $this->getCount();
-
-        // IMAGE File Location
-        $thmbImg = $results[$key]['thumbnail_image'];
-        $resImg = $results[$key]['restaurant_image'];
-        $imagePath = 'https'. '://' . $_SERVER['HTTP_HOST'] . '/images/restaurant/';
-        $filePath = '../../../images/restaurant/';
-
-        if(file_exists($filePath . $resImg)) {
-            $resImgUrl = $imagePath . $resImg;
-        }
-        if(file_exists($filePath . $thmbImg)) {
-            $thmbImgUrl = $imagePath . $thmbImg;
-        }  
+        // Get all Restaurants
+        $this->viewAllRestaurants();     
          
         // Adding data into Array
            $arr = array(
             'food_id' => $results[$key]['meal_id'],
             'food_name' => $results[$key]['food_name'],
-            'food_image' => $results[$key]['food_image'],      
+            'food_image' => $results[$key]['food_image'],
+            'restaurants' => $this->arrR  
            );
-
-           $arrR = [];
-
-           if($countReview > 0) {
-           foreach($reviews as $key => $values) {
-            $arrReview =[    
-            'name' => $reviews[$key]['name'],
-            'comment' => $reviews[$key]['review']
-            ];  
-            array_push($arrR, $arrReview);           
-        }  
-    }
-
-           $arr['restaurant'] = [
-            'country_name' => $results[$key]['country_name'],
-            'city_name' => $results[$key]['city_name'],
-            'restaurnat_name' => $results[$key]['restaurant_name'],
-            'restaurant_hours' => $results[$key]['restaurant_hours'],
-            'cuisine_name' => $results[$key]['cuisine_name'],
-            'cuisines' => $results[$key]['cuisines'],
-            'address' => $results[$key]['location'],
-            'establishment_name' => $results[$key]['name'],
-            'average_cost' => $results[$key]['average_cost'],
-            'restaurant_image' => $resImgUrl,
-            'thumbnail_image' => $thmbImgUrl,
-            'review' => $arrR
-           ];
-
            // Push Deals Data Into Data Array
            array_push($this->data, $arr);
        }
@@ -85,6 +44,47 @@ class DealView extends DealModel {
             "data" => $this->data    
             ]);
      }
+}
+
+    function viewAllRestaurants() {
+        $restaurant = $this->getResturant();
+        $countReview = $this->getCount();
+
+        $this->arrR = [];
+
+        if($countReview > 0) {
+        foreach($restaurant as $key => $values) {
+
+             // IMAGE File Location
+         $thmbImg = $restaurant[$key]['thumbnail_image'];
+         $resImg = $restaurant[$key]['restaurant_image'];
+         $imagePath = 'https'. '://' . $_SERVER['HTTP_HOST'] . '/images/restaurant/';
+         $filePath = '../../../images/restaurant/';
+ 
+         if(file_exists($filePath . $resImg)) {
+             $resImgUrl = $imagePath . $resImg;
+         }
+         if(file_exists($filePath . $thmbImg)) {
+             $thmbImgUrl = $imagePath . $thmbImg;
+         }  
+
+           $arrRes = [
+            'restaurant_id' => $restaurant[$key]['res_id'],
+            'country_name' => $restaurant[$key]['country_name'],
+            'city_name' => $restaurant[$key]['city_name'],
+            'restaurnat_name' => $restaurant[$key]['restaurant_name'],
+            'restaurant_hours' => $restaurant[$key]['restaurant_hours'],
+            'cuisine_name' => $restaurant[$key]['cuisine_name'],
+            'cuisines' => $restaurant[$key]['cuisines'],
+            'address' => $restaurant[$key]['location'],
+            'establishment_name' => $restaurant[$key]['name'],
+            'average_cost' => $restaurant[$key]['average_cost'],
+            'restaurant_image' => $resImgUrl,
+            'thumbnail_image' => $thmbImgUrl,
+           ];
+        array_push($this->arrR, $arrRes);
+        } 
+    }           
     }
 }
 
