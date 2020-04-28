db9aadb915cd00cef33b46e596a04ef978992936
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 14:09:44 +0545

    added code

---
+++ application/api/common/login.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json, text/html; charset=UTF-8');
+header('Access-Control-Allow-Method: POST');
+
+include '../../includes/autoloader-class.php';
+include '../../includes/utility.php';
+
+if(authAPI()) {
+    $userlogin = new LoginView();
+
+    if(isset($_POST['username']) && isset($_POST['password'])) {
+        if(empty(isEmpty())) {
+        echo $userlogin->viewUserDetails($_POST['username'], $_POST['password']);
+        } else {
+            echo isEmpty();
+        }
+    } 
+}
