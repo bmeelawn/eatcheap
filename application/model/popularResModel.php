d8f09f626a8e0da390efb71ecd2f66e5146f5f99
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 10:11:48 +0545

    added code

--- application/model/popularResModel.php
+++ application/model/popularResModel.php
@@ -5,7 +5,7 @@ include '../../config/database.php';
 class PopularResModel extends DBConnection {
     protected function getPopularRestaurant() {
         $sql = "SELECT c.id as country_id, c.name as country_name, ci.id as city_id, 
-        ci.name as city_name, r.city_id, r.most_popular, r.images, r.thumbnail_image,
+        ci.name as city_name, r.images, r.city_id, r.most_popular, r.images, r.thumbnail_image,
          r.restaurant_image, r.cuisines, r.restaurant_name, r.restaurant_hours, r.average_cost,
          r.location, cu.cuisine_name, cu.id, e.id, e.name, r.id as res_id
          FROM restaurants AS r
