e0593782f96109c2194f97e3aaf454c7c0f953cc
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 08:04:32 +0545

    added code

--- application/model/dealModel.php
+++ application/model/dealModel.php
@@ -4,18 +4,18 @@ include '../../config/database.php';
 
 class DealModel extends DBConnection{
     protected function getDeals() {
-        $sql = "SELECT m.id as meal_id, m.food_name, m.food_image, r.restaurant_name, r.deal_id,
+        $sql = "SELECT m.id as meal_id, m.food_name, m.food_image, r.restaurant_name, m.restaurant_id,
                     r.most_popular, r.images, r.thumbnail_image, r.id as res_id,
          r.restaurant_image, r.cuisines, r.restaurant_hours, r.average_cost, 
          r.location, cu.cuisine_name, cu.id, ci.name as city_name, c.name as country_name,
          e.name
-         FROM meals AS m
-        LEFT JOIN restaurants AS r ON m.id = r.deal_id
+         FROM restaurants AS r
+        LEFT JOIN meals AS m ON m.restaurant_id = r.id
         LEFT JOIN cuisines AS cu ON r.cuisine_id = cu.id
         LEFT JOIN cities AS ci ON r.city_id = ci.id
         LEFT JOIN countries AS c ON ci.con_id = c.id
         LEFT JOIN establishment AS e ON r.establishment_id = e.id
-        WHERE m.id = r.deal_id";
+        WHERE m.restaurant_id = r.id";
         $this->prepare($sql);
         $this->execute();
         return $this->resultSet();
@@ -23,13 +23,19 @@ class DealModel extends DBConnection{
     protected function getCount() {
         return $this->rowCount(); 
     }   
-    protected function getReviews($id) {
-        $sql = "SELECT re.review, re.name, r.id
-         FROM review AS re
-        LEFT JOIN restaurants AS r ON r.id = re.restaurant_id
-        WHERE r.id = ?";
+    protected function getResturant() {
+        $sql = "SELECT m.id as meal_id, m.food_name, m.food_image, r.restaurant_name, m.restaurant_id,
+        r.most_popular, r.images, r.thumbnail_image, r.id as res_id,
+        r.restaurant_image, r.cuisines, r.restaurant_hours, r.average_cost, 
+        r.location, cu.cuisine_name, cu.id, ci.name as city_name, c.name as country_name,
+        e.name
+        FROM meals AS m
+        LEFT JOIN restaurants AS r ON m.restaurant_id = r.id
+        LEFT JOIN cuisines AS cu ON r.cuisine_id = cu.id
+        LEFT JOIN cities AS ci ON r.city_id = ci.id
+        LEFT JOIN countries AS c ON ci.con_id = c.id
+        LEFT JOIN establishment AS e ON r.establishment_id = e.id";
         $this->prepare($sql);
-        $this->bindValue(1, $id);
         $this->execute();
         return $this->resultSet();
     }
