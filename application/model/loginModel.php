3c79fcfe2a5fe0c6dae1dde77a7d93c71ba8e684
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 13:18:25 +0545

    added code

--- application/model/loginModel.php
+++ application/model/loginModel.php
@@ -2,6 +2,23 @@
 
 include '../../config/database.php';
 
-class UserModel {
-    
+class LoginModel extends DBConnection {
+    private $username;
+    private $email;
+    private $firstname;
+    private $lastname;
+    private $image;
+    private $address;
+
+    protected function authUser($username) {
+        $sql = "SELECT * FROM users WHERE BINARY username = ?";
+        $this->prepare($sql);
+        $this->bindValue(1, $username);
+        $this->execute();
+        if($this->rowCount()) {
+            return $this->single();
+        } else {
+            return false;
+        }
+    }
 }
