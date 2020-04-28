4aea7b54938add4dbde72a0a24bc9d88b7c92195
bmeelawn <budhathokimilan4@gmail.com>
2020-04-28 10:11:34 +0545

    added code

--- application/model/popularFoodModel.php
+++ application/model/popularFoodModel.php
@@ -5,7 +5,7 @@ include '../../config/database.php';
 class PopularFoodModel extends DBConnection {
     protected function getPopularFoods() {
         $sql = "SELECT r.is_food_popular, r.food_image, r.restaurant_name,r.average_cost, r.id as res_id,
-         r.location, r.cuisines, r.special_item, cu.cuisine_name, cu.id, e.id, e.name
+         r.location, r.images, r.cuisines, r.special_item, cu.cuisine_name, cu.id, e.id, e.name
          FROM restaurants AS r
         LEFT JOIN cuisines AS cu ON  r.cuisine_id = cu.id
         LEFT JOIN establishment AS e ON  r.establishment_id = e.id
